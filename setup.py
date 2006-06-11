626fb1ad68ee9de3a230c63dfc43a846d8372bb7
Johann C. Rocholl <johann@rocholl.net>
2006-06-11 12:27:34 +0000

    Distutils.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name = 'PyPNG',
+      version = '0.3-alpha1',
+      description = 'Pure Python PNG encoder.',
+      author = 'Johann C. Rocholl',
+      author_email = 'johann@browsershots.org',
+      url = 'http://trac.browsershots.org/wiki/PyPng',
+      package_dir = {'': 'lib'},
+      py_modules = ['png'],
+      )
